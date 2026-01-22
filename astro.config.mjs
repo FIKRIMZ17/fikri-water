3daf67b3d6725e63e7860f9c0b8cb0c975b3103b
FIKRIMZ17 <fikrimuhamadj@gmail.com>
2026-01-22 15:27:29 +0700

    Siap launching fitur kupon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
+// File: astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
